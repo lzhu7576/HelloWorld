760f6879d23d4a5f010383a90cfc366a2149ae96
朱力 <lzhu@zhulideMacBook-Air.local>
2017-08-10 09:59:00 +0800

    First story finished

---
+++ HelloWorld/HelloWorld/SnippetData.swift
@@ -0,0 +1,15 @@
+//
+//  SnippetData.swift
+//  HelloWorld
+//
+//  Created by 朱力 on 2017/8/10.
+//  Copyright © 2017年 朱力. All rights reserved.
+//
+
+import Foundation
+
+struct SnippetData {
+    init(){
+        print("new snippet created")
+    }
+}
--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var data: [SnippetData] = [SnippetData]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func createNewSnippet(_ sender: Any) {
+        let newSnippet = SnippetData()
+        data.append(newSnippet)
+    }
 
 }
 
